eabe6f893251a77509111b37f228d22ca5012600
Fernando Gomes <lfgsnando@gmail.com>
2020-02-08 18:13:06 -0300

    Initial Commit

---
+++ EnglishDictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EnglishDictionary
+//
+//  Created by Fernando Gomes on 08/02/20.
+//  Copyright © 2020 Fernando Gomes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
